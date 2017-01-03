d44174071e6936233d903dd7cfd191e71eef8bb5
parinaz77 <parinaz77@gmail.com>
2017-01-03 15:04:42 -0800

    create repository

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -4,7 +4,6 @@ end
 
 post '/sessions'do
   @chatroom = Chatroom.authenticate(params[:room_name], params[:key])
-  p session[:user]
   if @chatroom
     if @chatroom.users[session[:user]] == nil
       @chatroom.users[session[:user]]=["red","blue","purple","orange","yellow","green"].sample
